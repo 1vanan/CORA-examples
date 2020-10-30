ea897c51142a536af3d4ef2c14c77205c0e42baf
Fedotov <vanen31@gmail.com>
2020-10-30 14:23:01 +0100

    add enclosing box

--- abstraction/examples/2RL_system/abstraction_RL_example.m
+++ abstraction/examples/2RL_system/abstraction_RL_example.m
@@ -3,7 +3,7 @@ function abstraction_RL_example()
 %     reachability analysis for 2-RL power system using cells set of inputs
 %    and states.
 %
-% Syntax:
+% Syntax:  
 %    abstraction_RL_example()
 %
 % Inputs:
@@ -12,7 +12,7 @@ function abstraction_RL_example()
 % Outputs:
 %    no
 %
-% Example:
+% Example: 
 %
 % References:
 %    -
@@ -31,36 +31,39 @@ alg = RL_Con;
 
 % Parameters and options for grid and algorythm ---------------------------
 % lower and upper bounds and error for 2-dim state space.
-state_gr_opt = grid_options([205 205], [206 206], [0.5 0.5]);
+state_gr_opt = grid_options([10 10], [11 11], [0.5 0.5]);
 % lower and upper bounds and error for 2-dim input space.
-input_gr_opt = grid_options([10 10], [11 11], [0.5 0.5]);
+input_gr_opt = grid_options([205 205], [206 206], [0.5 0.5]);
 r_params.tFinal = 0.25; % the final time for analysis.
 
 % one time step calculation. If the error is too big, will be decreased.
 r_options.timeStep = 0.01;
 % order of Taylor series expansion for the exp(A\delta t)
-r_options.taylorTerms = 20;
+r_options.taylorTerms = 20; 
 %upper bound = p/n, p - sum members, n - dimension.
-r_options.zonotopeOrder = 20;
+r_options.zonotopeOrder = 20; 
 % zonotope order is reduced to error order
-r_options.errorOrder = 200;
+r_options.errorOrder = 200; 
 % order of the Taylor series expansion. Default is 2
-r_options.tensorOrder = 2;
+r_options.tensorOrder = 2; 
 % max overall error. default - infinity
-r_options.maxError = [10; 10];
+r_options.maxError = [10; 10]; 
 % max dif. error. default - infinity
-r_options.maxError_x = r_options.maxError;
+r_options.maxError_x = r_options.maxError; 
 % max alg. error. default - infinity
-r_options.maxError_y = 10;
+r_options.maxError_y = 10; 
 
 % Reachability Analysis ---------------------------------------------------
-% path for saving result.
+% path to save result.
 path = "./resources/reachability_res.json";
+% provide one more argument, "false", if disable of enclosing box is needed
 solver = reach_solver(state_gr_opt, input_gr_opt, ...
     r_params, r_options);
 solver.compute(path, alg, dif);
 
 % Visualization -----------------------------------------------------------
+%TODO
 end
 
 %------------- END OF CODE --------------
+
--- abstraction/reach_solver.m
+++ abstraction/reach_solver.m
@@ -1,10 +1,10 @@
 classdef reach_solver
-
+    
 % reach_solver - compute reachability zonotopes for all possible
 % permutations of input ans state cells.
 %
-% Syntax:
-%    obj = reachability(algebraic_system, differential_system,
+% Syntax:  
+%    obj = reachability(algebraic_system, differential_system, 
 %    state_grid_options, input_grid_options, r_params, r_options)
 %
 % Inputs:
@@ -23,69 +23,77 @@ classdef reach_solver
 % Subfunctions: none
 % MAT-files required: none
 %
-% See also:
+% See also: 
 % https://gitlab.lrz.de/matthias/SCOTSv0.2/-/blob/master/src/Abstraction.hh
 
 % Author:       Ivan Fedotov
-% Written:      21-October-2020
+% Written:      21-October-2020 
 % Last update:  TODO: guess y0 and parallelalize reach loops with "parfor"
-% improve printing progress
+% improve printing progress 
 % Last revision: ---
 
 %------------- BEGIN CODE --------------
-
+    
     properties (GetAccess = public, SetAccess = private)
-        % state grid options must include the following n-dimensional
+        % state grid options must include the following n-dimensional 
         % arrays: lower_bound, upper_bound, err.
         state_grid_options;
-
+        
         % input grid options.
         input_grid_options;
-
+        
         % reachability params must include: tFinal, y0guess, R0, U.
         r_params
-
-        % reachability options must include: timeStep, taylorTerms,
-        % zonotopeOrder,errorOrder, tensorOrder, maxError, maxError_x,
+        
+        % reachability options must include: timeStep, taylorTerms, 
+        % zonotopeOrder,errorOrder, tensorOrder, maxError, maxError_x, 
         % maxError_y.
         r_options
+        
+        % put this flag in 'true' if writing to json enclosingaxis-aligned 
+        % box, but not the whole zonotope is necessary. True by default.
+        using_intervals;
     end
-
+    
     methods
-        function obj = reach_solver(state_grid_options,  ...
-                input_grid_options, r_params, r_options)
-
-            obj.state_grid_options = state_grid_options;
-            obj.input_grid_options = input_grid_options;
-            obj.r_params = r_params;
-            obj.r_options = r_options;
+        function obj = reach_solver(varargin)
+            obj.state_grid_options = varargin{1};
+            obj.input_grid_options = varargin{2};
+            obj.r_params = varargin{3};
+            obj.r_options = varargin{4};
+            
+            if nargin > 4
+                obj.using_intervals = varargin{5};
+            else
+                obj.using_intervals = true;
+            end
         end
-
+        
         function result = compute(obj, path, algebraic_s, differential_s)
             state_grid = uniformGrid(obj.state_grid_options);
             input_grid = uniformGrid(obj.input_grid_options);
             result = 0;
-
-            % structure for JSON.
+            
+            % final step of zonotope calculation
+            fin_step = obj.r_params.tFinal / obj.r_options.timeStep;
+            
+            % structure for JSON. 
             S = struct();
-
+            
             %zonotope for disturbance. consist of only the center.
             %Represents the error - size of cell.
             state_cells_amount = state_grid.total_cells;
             error_matrix = zeros(state_grid.dim, state_grid.dim);
             error_matrix(:,1) = obj.state_grid_options.err;
             obj.r_params.U = zonotope(error_matrix);
-
-
+            
+            
 % Reachability Analysis----------------------------------------------------
-
-            disp("start reachability computations");
-
             % loop over all states
-            for i = 0 : state_cells_amount
+            for i = 0 : state_cells_amount - 1
                 % print progress
                 disp(".");
-
+                
                 state = state_grid.itox(i);
                 state_matrix = zeros(state_grid.dim, state_grid.dim + 1);
                 % center of the zonotope is a state
@@ -97,71 +105,74 @@ classdef reach_solver
                     obj.state_grid_options.err(k-1)/2;
                 end
                 % loop over all inputs
-                for j = 0 : input_grid.total_cells
+                for j = 0 : input_grid.total_cells - 1
                     inputs = input_grid.itox(j);
-
+                    
                     %put input into equations
                     differential_s.inputs = inputs;
                     algebraic_s.inputs = inputs;
-
+                    
                     % Guess for algebraic part TODO: how to guess it?
                     obj.r_params.y0guess = 33;
                     % zonotope for initial dif vars
                     obj.r_params.R0 = zonotope(state_matrix);
-
+                    
                     powerDyn = nonlinDASys(@differential_s.dyn_eq, ...
                         @algebraic_s.con_eq, ...
                         state_grid.dim, input_grid.dim, ...
                         algebraic_s.constraints);
-
+                    
 % Reachability Analysis  for one cell--------------------------------------
                     R = reach(powerDyn, obj.r_params, obj.r_options);
-
-
+                    
 % Add Result To JSON ------------------------------------------------------
-                    % amount of generators for current iteration
-                    temp_array = size(R.timePoint.set{1, 1}.Z);
-                    gen_nums = temp_array(2); % TODO: avoid null gen-s
-
+                    if obj.using_intervals
+                        Z = box(R.timePoint.set{fin_step, 1});
+                    else
+                        Z = R.timePoint.set{fin_step, 1};
+                    end
+                    
+                    % amount of generators for the current iteration
+                    temp_array = size(Z.Z);
+                    gen_nums = temp_array(2);
+                    
                     % generators for reachable set for current iteration
                     % generators = zeros(state_grid.dim, gen_nums);
                     for g = 2 : gen_nums
-                        gen = R.timePoint.set{1, 1}.Z(:,g);
-                        % avoid empty generators
+                        gen = Z.Z(:,g);
+                        % avoid empty generators 
                         if nnz(gen) ~= 0
                             for d = 1 : state_grid.dim
                                 generators(g-1, d) = gen(d);
                             end
                         end
                     end
-
-
-
+                    
                     %create sceleton for the whole json for the first time.
-                    % { "zonotopes":
-                    % [ { "input": [...], "state": [...], value:
-                    % { "center": [...], "generators": [...],...,[...] } },
+                    % { "zonotopes": 
+                    % [ { "input": [...], "state": [...], value: 
+                    % { "center": [...], "generators": [...],...,[...] } }, 
                     %   {...}, ..., {...}]}
                     if (j == 0 && i == 0)
                         S = struct ("zonotope", struct("input", ...
                         inputs, "state", state, "value",struct("center",...
-                        R.timePoint.set{1, 1}.Z(:,1), ...
+                        Z.Z(:,1), ...
                         "generators", generators)));
-                    else
-                        % add to structure the current reachable zonotope
-                        % and meta info:
+                    else 
+                        % add to structure the current reachable zonotope  
+                        % and meta info: 
                         S(end+1) = struct ("zonotope", struct("input",...
                         inputs, "state", state, "value",struct("center",...
-                        R.timePoint.set{1, 1}.Z(:,1), ...
+                        Z.Z(:,1), ...
                         "generatots", generators)));
                     end
                 end
-                % create json and write it to the file
+                % create json and write it to the file    
                 s = jsonencode(S);
                 fileID = fopen(path, 'w');
                 fprintf(fileID, s);
                 fclose(fileID);
-            end
+            end 
         end
     end
 end
